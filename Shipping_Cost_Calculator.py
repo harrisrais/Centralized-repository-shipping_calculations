6c67ffc574bee2d9fde855adc79182ff75f4b848
harrisrais <hr.rais.01@gmail.com>
2025-06-13 07:34:37 -0400

    Revert "added additional line to Shipping_Cost_Calculator.py"
    This reverts commit 783f644c9c64f78d3a4354b08e5288726c86217e.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,5 +1,4 @@
-# Here is another update by harrisrais
-# Here is a new update by harrisrais
+ # Here is a new update by harrisrais
 # Shipping Cost Calculator
 
 ## Input package weight and shipping rate
